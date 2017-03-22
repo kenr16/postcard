8f7ac0b658fd1ca8b349e2f31c4d86fa69103e48
Ken Rutan and Nick Powell <ken@>
2017-03-22 10:22:56 -0700

    add shout

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,11 @@ $(document).ready(function() {
   $("#formOne").submit(function(event) {
     var person1Input = $("input#person1").val();
     var concern1Input = $("input#concern1").val();
+    var cONCERNINPUT = concern1Input.toUpperCase();
+
 
     $(".person1").text(person1Input);
-    $(".concern1").text(concern1Input);
+    $(".concern1").text(cONCERNINPUT);
 
     $("#story").show();
 
